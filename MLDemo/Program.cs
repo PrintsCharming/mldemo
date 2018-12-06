855c55982004beffa93a3d514caf453a9036ee27
Ryan Smith <ryans@ocgoodwill.org>
2018-12-05 20:13:55 -0800

    Add project files.

---
+++ MLDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
